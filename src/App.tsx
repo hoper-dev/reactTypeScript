4ed13c822676bda6ee2eb452c0f0c6fdfae0550c
naokisato <67621329+hoper-dev@users.noreply.github.com>
2022-10-06 14:29:44 +0900

    axiosで会津若松市の観光データを取得

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,52 @@
 import React from "react";
 import { styled } from "@mui/material/styles";
 import Button from "@mui/material/Button";
- 
+import axios from "axios";
+
+const url =
+  "https://developers-api-aizuwakamatsu-p-mylocal.jp/mgmt/trip/spotlist/v1";
+
 const App: React.FC = () => {
-  return (
-    <Button
-      variant="contained"
-      sx={{
-        backgroundColor: "red",
-        "&:hover": {
-          backgroundColor: "red",
+  const apiFunc = () => {
+    console.log("test");
+    const req = url + "?langCode=ja_JP&sortType=1&pageNo=1";
+    axios
+      .get(req, {
+        headers: {
+          "Content-Type": "application/json",
+          "Ocp-Apim-Subscription-Key": process.env.REACT_APP_SUBSCRIPTION_KEY,
         },
-      }}
-    >
-      ボタン
-    </Button>
+      })
+      .then((res) => {
+        console.log("res", res);
+        //スポットリストのspotNameを配列に格納し、HTMLに表示(forとか使ってリスト表示する)
+        // spotname summary
+        // spotname summary
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  };
+  return (
+    <div>
+      <Button
+        onClick={apiFunc}
+        variant="contained"
+        sx={{
+          backgroundColor: "red",
+          "&:hover": {
+            backgroundColor: "red",
+          },
+        }}
+      >
+        データを取得する
+      </Button>
+
+      <div>
+        <ul>ここにリストでデータを表示する</ul>
+      </div>
+    </div>
   );
 };
- 
+
 export default App;
